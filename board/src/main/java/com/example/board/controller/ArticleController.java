fe09900a1d5ef8227474299a0cac4e5cdff2452c
with66780 <test@test.com>
2023-11-07 10:17:09 +0900

    게시글 페이지 만들기

--- board/src/main/java/com/example/board/controller/ArticleController.java
+++ board/src/main/java/com/example/board/controller/ArticleController.java
@@ -1,7 +1,10 @@
 package com.example.board.controller;
 
+import lombok.Getter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -10,9 +13,16 @@ import java.util.List;
 @Controller
 public class ArticleController {
 
-    @RequestMapping
+    @GetMapping
     public String articles(ModelMap map){
         map.addAttribute("articles", List.of());
         return "articles/index";
+    }
+
+    @GetMapping( {"/{articleId}"})
+    public String article(@PathVariable Long articleId, ModelMap map){
+        map.addAttribute("article", "article");
+        map.addAttribute("comments", List.of());
+        return "articles/detail";
     }
 }
